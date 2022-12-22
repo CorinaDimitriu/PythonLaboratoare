235f795e28f51efdc97ae89794773c0e20dc3747
CorinaDimitriu <79191386+CorinaDimitriu@users.noreply.github.com>
2022-12-22 16:17:43 +0200

    Almost finished: a bit more testing required...

---
+++ messages.py
@@ -0,0 +1,9 @@
+Success = "Operation succeeded"
+FileNotFound = "Error: Requested file does not exist or you do not have privilege to read from or write in it..."
+FileFormat = "Error: Requested file does not have appropriate format to perform requested operations on it"
+DatabaseManipulation = "Error: Database constraints or permissions violated..."
+CryptographicError = "Error: Wrong data types or lengths during cryptographic computations..." \
+                     "Please pay attention to the input data of the commands you issued!"
+ParameterSpecification = "Error: Misunderstanding of the significance of the commands' parameters..."
+Unknown = "Error: Some weird computations have been made and led to unpredicted exceptions..." \
+          "Please tell us more about the problem you encountered so that we can figure it out together!"
